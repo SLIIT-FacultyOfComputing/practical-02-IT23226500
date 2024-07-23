0addeae1888b90d1d447c6955fa11888ffa70ef8
IT23226500 <140576136+IT23226500@users.noreply.github.com>
2024-07-23 14:27:03 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class first{
+  
+  public static void main(String args[]){
+     Scanner scanner = new Scanner(System.in);
+     
+     System.out.print("Enter your First name: ");
+     String firstName = scanner.nextLine();
+     
+     System.out.print("Enter your Middle name: ");
+     String middleName = scanner.nextLine();
+     
+     System.out.print("Enter your Last name: ");
+     String lastName = scanner.nextLine();
+     
+     String fullName = new String (lastName + " " + firstName + " " + middleName);
+     //String[] parts = fullName.split(" "); 
+     
+     //System.out.println(lastName + ", " + firstName + " " + middleName);
+     System.out.println("Full name: "+ fullName);
+     
+     System.out.println("Enter another name");
+     String fullName2 = scanner.nextLine();
+     }
+}
+ 
+     
+    
+     
